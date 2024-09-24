ddbb285045d253463041e25d73b49f38f811b0fc
gottajsx <gottajsx@gmail.com>
2024-09-24 21:11:30 +0200

    New branch for setstate approach

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import Main from './Main'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Component1 from './Component1';
+import Component2 from './Component2';
+import Component3 from './Component3';
 
 function App() {
+  const [showMenu, setShowMenu] = useState(true);
+
   return (
     <Router>
-      <Main />
+      <div>
+        {showMenu && (
+          <nav>
+            <ul>
+              <li>
+                <Link to="/composant1" onClick={() => setShowMenu(false)}>
+                  Afficher Composant 1
+                </Link>
+              </li>
+              <li>
+                <Link to="/composant2" onClick={() => setShowMenu(false)}>
+                  Afficher Composant 2
+                </Link>
+              </li>
+              <li>
+                <Link to="/composant3" onClick={() => setShowMenu(false)}>
+                  Afficher Composant 3
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        )}
+
+        <hr />
+
+        <Routes>
+          <Route path="/composant1" element={<Component1 />} />
+          <Route path="/composant2" element={<Component2 />} />
+          <Route path="/composant3" element={<Component3 />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
